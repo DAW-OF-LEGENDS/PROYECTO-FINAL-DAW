b8ac7cb9856ce1a12e4ee1b8f4dffb462e490396
MoisesJPG <mojupega1610@gmail.com>
2025-03-28 12:37:03 +0100

    Pagina Recetary

--- src/js/script.js
+++ src/js/script.js
@@ -14,10 +14,5 @@ function changeThemeMode(update = false) {
 document.addEventListener('mousemove', (event) => {
     let X = event.clientX / window.innerWidth * 100;
     let Y = event.clientY / window.innerHeight * 100;
-    document.body.style.backgroundPosition = `${X}% ${Y}%`;
-    if(darkMode){
-        document.body.style.setProperty('--custom-bg', `radial-gradient(circle 256px at ${X}% ${Y}%, transparent 50%, rgba(0,0,0,0.125) 100%)`);
-    }else{
-        document.body.style.setProperty('--custom-bg', `radial-gradient(circle 256px at ${X}% ${Y}%, transparent 50%, rgba(255,255,255,0.125) 100%)`);
-    }
+    document.documentElement.style.setProperty(`--background-cursor-pos`, `${X}% ${Y}%`)
 });

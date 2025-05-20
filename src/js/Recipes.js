b036d0752b6bd1ae63f6491888fdaaaaf721a119
AdriSoup <a.puerto.f99@gmail.com>
2025-05-20 15:59:15 +0200

    Adición de recetas para testing

--- src/js/Recipes.js
+++ src/js/Recipes.js
@@ -17,9 +17,13 @@ export class Recipe {
  * @type {Recipe[]}
  */
 export const Recipes = [
-    new Recipe(Items["ALTERNADOR HEXTECK"], [Items["TOMO AMPLIFICADOR"], Items["TOMO AMPLIFICADOR"]]),
-    new Recipe(Items["SOBRECARGA TORMENTOSA"], [Items["ALTERNADOR HEXTECK"], Items["BRISA DE ETER"]]),
+    new Recipe(Items["ALTERNADOR HEXTECH"], [Items["TOMO AMPLIFICADOR"], Items["TOMO AMPLIFICADOR"]]),
+    new Recipe(Items["SOBRECARGA TORMENTOSA"], [Items["ALTERNADOR HEXTECH"], Items["BRISA DE ETER"]]),
     new Recipe(Items["BRISA DE ETER"], [Items["TOMO AMPLIFICADOR"]]),
+    new Recipe(Items["ROBAALMAS DE MEJAI"], [Items["SELLO OSCURO"]]),
+    new Recipe(Items["LUCIDEZ CARMESI"], [Items["BOTAS JONIAS DE LA LUCIDEZ"], Items ["BOTAS"], Items["MOTA BRILLANTE"]]),
+    new Recipe(Items["AVANCE ETERNO"], [Items["ALMAS EN SINCRONIA"], Items["SUELAS SIMBIOTICAS"], Items["BOTAS"]]),
+    
 ]
 export function findRecipeByResult(item){
     for(const recipe of Recipes){
